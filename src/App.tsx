f4d1500be75b1cae33a9a3aa967e0b8d532afa5d
Pierre Haller <pierre@marmelab.com>
2021-04-01 09:42:55 +0200

    Init repo

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  const [count, setCount] = React.useState(0);
+
+  return <div className="App"></div>;
+};
+
+export default App;
